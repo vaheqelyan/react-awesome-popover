89ae32cae8d7eb17a12d1594982f2fde560aac50
Vahe <martinkenner@mail.com>
2017-11-17 15:58:38 +0400

    update peerDependencies

---
+++ src/TargetComponent.js
@@ -0,0 +1,74 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Target } from "react-popper";
+
+export default class TargetComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.click = this.click.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+  }
+  onMouseLeave(e) {
+    const getElement = e.relatedTarget;
+    if (getElement && getElement.nodeName) {
+      const close = getElement.closest(".manager");
+      if (close) {
+        const hasDataId = close.hasAttribute("data-target-id");
+        if (hasDataId) {
+          const getDataId = close.getAttribute("data-target-id");
+          if (getDataId) {
+            if (getDataId != this.props.id) this.props.closePopover();
+          }
+        }
+      }
+    }
+  }
+  onMouseEnter() {
+    this.props.openPopover();
+  }
+  componentDidMount() {
+    const { action } = this.props;
+    const target = ReactDOM.findDOMNode(this);
+    this.target = target;
+    if (action === "click") {
+      target.addEventListener("click", this.click, false);
+    } else if (action === "hover") {
+      target.addEventListener("mouseenter", this.onMouseEnter, false);
+      target.addEventListener("mouseleave", this.onMouseLeave, false);
+    }
+  }
+  componentWillUnmount() {
+    const { action, closePopover } = this.props;
+    if (action === "click") {
+      this.target.removeEventListener("click", this.click, false);
+    } else if (action === "hover") {
+      this.target.removeEventListener("mouseenter", this.onMouseEnter, false);
+      this.target.removeEventListener("mouseleave", this.onMouseLeave, false);
+    }
+  }
+  click(e) {
+    e.stopImmediatePropagation();
+    const close = e.target.closest(".popover-content");
+    if (close) {
+      const getpopover = close.querySelector(".popover-content");
+      if (!getpopover) {
+        this.props.tooglePopover();
+      }
+    } else {
+      this.props.tooglePopover();
+    }
+  }
+  render() {
+    return (
+      <Target>
+        {({ targetProps }) => (
+          <div className="target-container" {...targetProps}>
+            {this.props.children}
+          </div>
+        )}
+      </Target>
+    );
+  }
+}
