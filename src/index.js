5266320eb0b41c7ece9dc04d257c226576da298d
Vahe <martinkenner@mail.com>
2018-02-20 20:30:05 +0400

    -0,0 +1,2  -2 +4,14

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import Popover from "./Popover";
-export default Popover;
+
+ReactDOM.render(
+  <Popover index={1000} placement="left">
+    <button>Hello worldllllllllllllll..........</button>
+    <div>
+      <p>Hello world.......................................</p>
+      <Popover index={1010}>
+        <button>Click</button>
+        <div>Popover content</div>
+      </Popover>
+    </div>
+  </Popover>,
+  document.getElementById("app")
+);
