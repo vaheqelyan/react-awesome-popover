4bd691626845a2f2141c1bff77c3e2bb766405ba
Vahe <martinkenner@mail.com>
2018-02-20 20:31:05 +0400

    -100 +100

--- src/PopoverComponent.js
+++ src/PopoverComponent.js
@@ -97,7 +97,7 @@ export default class PopoverComponent extends React.Component {
             } else if (/top/gi.test(popperProps["data-placement"])) {
               popperProps.style.top -= 8.4;
             } else if (/left/gi.test(popperProps["data-placement"])) {
-              popperProps.style.top += 8.4;
+              popperProps.style.left += 8.4;
             }
             popperProps.className = "popover-content";
             popperProps.style.zIndex = this.props.zIndex + 10;
