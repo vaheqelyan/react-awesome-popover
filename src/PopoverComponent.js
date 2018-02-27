f742cf7d7d310cd4b3ae651b6d4d5f9a03b8f42d
Vahe <martinkenner@mail.com>
2018-02-27 17:03:36 +0400

    -6,33 +5,0

--- src/PopoverComponent.js
+++ src/PopoverComponent.js
@@ -3,39 +3,6 @@ import { Popper } from "react-popper";
 import ArrowComponent from "./ArrowComponent";
 import isOverlay from "./isOverlay";
 
-class Overlay extends React.Component {
-  componentDidMount() {
-    const { touch, onClosePopover } = this.props;
-    if (touch) {
-      this.refs.node.addEventListener("touchstart", onClosePopover, false);
-    } else {
-      this.refs.node.addEventListener("click", onClosePopover, false);
-    }
-  }
-  componentWillUnmount() {
-    const { touch, onClosePopover } = this.props;
-    if (touch) {
-      this.refs.node.removeEventListener("touchstart", onClosePopover, false);
-    } else {
-      this.refs.node.removeEventListener("click", onClosePopover, false);
-    }
-  }
-
-  render() {
-    const { onClosePopover, zIndex } = this.props;
-    return (
-      <div
-        ref="node"
-        onClick={onClosePopover}
-        id="rap-overlay"
-        style={{
-          zIndex: this.props.zIndex + 100
-        }}
-      />
-    );
-  }
-}
-
 export default class PopoverComponent extends React.Component {
   constructor(props) {
     super();
