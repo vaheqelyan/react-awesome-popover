20f3f4661ecf73d60b2465ea9f06be9d3a6b4055
Vahe <martinkenner@mail.com>
2018-07-04 17:47:54 +0400

    Check whether the node is null

--- src/isOverlay.js
+++ src/isOverlay.js
@@ -1,6 +1,8 @@
 export default function isOverlay(node) {
-  if (node.nodeName == "DIV" && node.id == "rap-overlay") {
-    return true;
-  }
-  return false;
+	if (node) {
+		if (node.nodeName == "DIV" && node.id == "rap-overlay") {
+			return true;
+		}
+	}
+	return false;
 }
