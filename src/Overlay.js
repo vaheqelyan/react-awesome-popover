b32057a1048402be057ac759db16806db119d8b3
Vahe <martinkenner@mail.com>
2018-09-03 15:43:26 +0400

    Made the component functional and removed life cycles

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,34 +1,15 @@
-import React from "react";
+import React from 'react';
 
-export default class Overlay extends React.Component {
-  componentDidMount() {
-    const { touch, onClosePopover } = this.props;
-    if (touch) {
-      this.refs.node.addEventListener("touchstart", onClosePopover, false);
-    } else {
-      this.refs.node.addEventListener("click", onClosePopover, false);
-    }
-  }
-  componentWillUnmount() {
-    const { touch, onClosePopover } = this.props;
-    if (touch) {
-      this.refs.node.removeEventListener("touchstart", onClosePopover, false);
-    } else {
-      this.refs.node.removeEventListener("click", onClosePopover, false);
-    }
-  }
+export default function Overlay({ onClosePopover, zIndex, touch }) {
+  let props = { [touch ? 'onTouchStart' : 'onMouseDown']: onClosePopover };
 
-  render() {
-    const { onClosePopover, zIndex } = this.props;
-    return (
-      <div
-        ref="node"
-        onClick={onClosePopover}
-        id="rap-overlay"
-        style={{
-          zIndex: this.props.zIndex + 100
-        }}
-      />
-    );
-  }
+  return (
+    <div
+      id="rap-overlay"
+      style={{
+        zIndex: zIndex + 100
+      }}
+      {...props}
+    />
+  );
 }
