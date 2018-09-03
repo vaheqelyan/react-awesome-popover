7b4b31ae44bd2b94d8735c0f7bd94e834393569d
Vahe <martinkenner@mail.com>
2018-09-03 15:43:32 +0400

    Made the component functional and made optimization

--- src/ArrowComponent.js
+++ src/ArrowComponent.js
@@ -1,73 +1,48 @@
-import React from "react";
-import { Arrow } from "react-popper";
-import randomID from "random-id";
+import React from 'react';
+import { Arrow } from 'react-popper';
 
-class ArrowComponent extends React.Component {
-  constructor() {
-    super();
-    this.id = randomID(19, "a");
-  }
-  render() {
-    const { dataPlacement, customArrow, arrowClass } = this.props;
-    const { id } = this;
-    return (
-      <Arrow>
-        {({ arrowProps }) => {
-          arrowProps.style.position = "absolute";
-          if (/right/gi.test(dataPlacement)) {
-            arrowProps.style.transform = "rotate(-180deg)";
-            arrowProps.style.left = "-19px";
-          } else if (/bottom/gi.test(dataPlacement)) {
-            arrowProps.style.transform = "rotate(-90deg)";
-            arrowProps.style.top = "-20px";
-          } else if (/top/gi.test(dataPlacement)) {
-            arrowProps.style.transform = "rotate(90deg)";
-            arrowProps.style.bottom = "-21px";
-          } else if (/left/gi.test(dataPlacement)) {
-            arrowProps.style.right = "-19px";
+export default function ArrowComponent({ dataPlacement, customArrow, arrowClass }) {
+  return (
+    <Arrow>
+      {({ arrowProps }) => {
+        let g = dataPlacement && dataPlacement.split('-')[0];
+        arrowProps.style.position = 'absolute';
+
+        arrowProps.style[
+          do {
+            if (g == 'right') {
+              ('left');
+            } else if (g == 'left') {
+              ('right');
+            } else if (g == 'bottom') {
+              ('top');
+            }
           }
+        ] =
+          '-8px';
 
-          return customArrow ? (
-            <span {...arrowProps} className={arrowClass}>
-              {customArrow}
-            </span>
-          ) : (
-            <span {...arrowProps} className={arrowClass}>
-              <svg
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                viewBox="0 0 100 100"
-                version="1.1"
-                x="0px"
-                y="0px"
-                width={30}
-                height={30}
-              >
-                <defs xmlns="http://www.w3.org/2000/svg">
-                  <filter id={id} height="130%">
-                    <feGaussianBlur in="SourceAlpha" stdDeviation={3} />
-                    <feOffset dx={7} dy={1} result="offsetblur" />
-                    <feComponentTransfer>
-                      <feFuncA type="linear" slope="0.2" />
-                    </feComponentTransfer>
-                    <feMerge>
-                      <feMergeNode />
-                      <feMergeNode in="SourceGraphic" />
-                    </feMerge>
-                  </filter>
-                </defs>
-                <polygon
-                  filter={`url(#${id})`}
-                  points="36 23 64 55 36 80"
-                  fill="#fff"
-                  fillRule="evenodd"
-                />
-              </svg>
-            </span>
-          );
-        }}
-      </Arrow>
-    );
-  }
+        return customArrow ? (
+          <span {...arrowProps} className={arrowClass}>
+            {customArrow}
+          </span>
+        ) : (
+          <span {...arrowProps}>
+            <div
+              className={`arrow-${do {
+                if (g == 'left') {
+                  ('right');
+                } else if (g == 'right') {
+                  ('left');
+                } else if (g == 'top') {
+                  ('bottom');
+                } else if (g == 'bottom') {
+                  ('up');
+                }
+              }}`}
+            />
+          </span>
+        );
+      }}
+    </Arrow>
+  );
 }
-
-export default ArrowComponent;
