33cfb768a33be114b014d66b08cff88860082c89
vaheqelyan <martinkenner@mail.com>
2019-09-22 15:33:01 +0400

    Formatting source

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,14 +1,6 @@
 import React from "react";
 
-const Overlay = ({
-	onClose,
-	overlayColor = "rgba(0,0,0,0.5)",
-	zIndex,
-	action,
-	setOpen,
-	preventDefault,
-	stopPropagation,
-}) => {
+const Overlay = ({ onClose, overlayColor = "rgba(0,0,0,0.5)", zIndex, action, setOpen, preventDefault, stopPropagation }) => {
 	const onClick = e => {
 		if (preventDefault) e.preventDefault();
 		if (stopPropagation) e.stopPropagation();
